b8215ba1ad10db72689a904b3384891203b77579
Firore <takbasic722@gmail.com>
2019-07-31 19:45:01 +0700

    up

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <!DOCTYPE HTML>
 <html lang="en">
-  <head>
-	  <?php //แก้ content เป็น client id ที่ config ไว้กับ google?>
-	<meta name="google-signin-client_id" content="977916372948-p4d07ppqrn4i4m5paush7crr6qvicscq.apps.googleusercontent.com">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-     <?php 
-            include 'inc_js.php';  
-      ?>
+    <head>
+        <?php //แก้ content เป็น client id ที่ config ไว้กับ google?>
+            <meta name="google-signin-client_id" content="977916372948-p4d07ppqrn4i4m5paush7crr6qvicscq.apps.googleusercontent.com">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+         <?php 
+                include 'inc_js.php';  
+          ?>
 
-    <title>เข้าสู่ระบบ</title>
+        <title>เข้าสู่ระบบ</title>
 
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="CSS/bootstrap.css">
-    <link rel="stylesheet" href="CSS/floating-labels.css">
-    <link rel="stylesheet" href="CSS/sticky-footer.css">
-    <!-- Custom styles for this template -->
-    <link rel="stylesheet" href="CSS/jquery-ui.css">
-    <link rel="stylesheet" href="CSS/main.css">
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/floating-labels.css">
+        <link rel="stylesheet" href="CSS/sticky-footer.css">
+        <!-- Custom styles for this template -->
+        <link rel="stylesheet" href="CSS/jquery-ui.css">
+        <link rel="stylesheet" href="CSS/main.css">
 
-  </head>
+    </head>
 
-  <body>
-<?php
+    <body>
+        <?php
 
-    include  'login.php';
-?>
- </body>
+            include  'login.php';
+        ?>
+    </body>
 </html>
