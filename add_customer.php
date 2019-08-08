b9de52703808b375fd2c0d4a1fa5dab8819b1e5e
Firore <takbasic722@gmail.com>
2019-08-09 00:06:12 +0700

    up

--- add_customer.php
+++ add_customer.php
@@ -2,6 +2,7 @@
     include "tools/ddd.php";
     $form = new form();
     $lbname = new label('ชื่อ-นามสกุล');
+    $lbnickname = new label('ชื่อเล่น');
     $lbposition = new label('ตำแหน่ง');
     $lbaddress = new label('ที่อยู่');
     $lbshop = new label('ชื่อร้าน');
@@ -11,6 +12,7 @@
     $lbdistrict = new label('เขต/อำเภอ');
     $lbprovince = new label('จังหวัด');
     $txtname = new textfield('customer_name','','form-control','','');
+    $txtnickname = new textfield('customer_name','','form-control','','');
     $txtposition = new textfield('customer_position','','form-control','','');
     $txtaddress = new textfield('customer_address','','form-control','','');
     $txtshop = new textfield('customer_shop','','form-control','','');
@@ -19,169 +21,183 @@
     $txtsubdistrict = new textfield('customer_subdistricts','','form-control','','');
     $txtdistrict = new textfield('customer_districts','','form-control','','');
     $txtprovince = new textfield('customer_provinces','','form-control','','');
-    $submit = new buttonok('บันทึก ลูกค้า','bs','btn btn-success col-12','');
+    $submit = new buttonok('ยินยันการส่งข้อมูล','bs','btn btn-success col-12','');
     $token = new tokens();
-    $tk = $token->openToken();
-   echo $form->open("","","col-12","insert_customer.php","");
-   ?>
+    $tk = $token->openToken();?>
 <div class="row">
+<div class="col-12 bg1">
+  <div class="row">
     <div class="col-4"></div>
-    <div class="col-4 mt-5">
+    <div class="col-4">
         <div class="row">
-            <div class="col-12">
-                <div class="row">
-                    <h3>เพิ่มลูกค้า</h3>
+        <div class="col-12 pt-3 pb-2 tx2">
+           <div class="row">
+                <h2 class="pl-3">ลงทะเบียนรายชื่อผู้ติดต่อ</h2>
+           </div>
+        </div>
+    <?php
+    echo $form->open("","","col-12 tx1","insert_customer.php","");
+    ?>
+        <div class="col-12 mt-3">
+            <div class="row">
+                <div class="col-12 tx2">
+                    <div class="row">
+                            <?php echo $lbname;  ?>
+                    </div>
                 </div>
-            </div>
-            <div class="col-12 mt-4">
-                <div class="row">
-                        <?php echo $lbname;  ?>
+                <div class="col-12">
+                    <div class="row">
+                            <?php echo $txtname;  ?>
+                    </div>
                 </div>
-            </div>
-            <div class="col-12">
-                <div class="row">
-                        <?php echo $txtname;  ?>
-                </div>
-            </div>
-            <div class="col-12 pb-4">
-                <div class="row">
-                    <div class="col-6 mt-2">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="row">
-                                    <?php echo $lbposition;  ?>
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-6 mt-2">
+                            <div class="row">
+                                <div class="col-12 tx2">
+                                    <div class="row">
+                                        <?php echo $lbposition;  ?>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="row">
-                                    <?php echo $txtposition;  ?>
+                                <div class="col-12">
+                                    <div class="row">
+                                        <?php echo $txtposition;  ?>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-6 mt-2">
-                        <div class="row">
-                            <div class="col-12 pr-0">
-                                <?php echo $lbshop;  ?>
-                            </div>
-                            <div class="col-12 pr-0">
-                                <?php echo $txtshop;  ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="row">
-                    <h3>เพิ่มข้อมูลการติดต่อ</h3>
-                </div>
-            </div>
-            <div class="col-12 mt-3">
-                <div class="row">
-                    <div class="col-6 mt-2">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="row">
-                                    <?php echo $lbtel;  ?>
+                        <div class="col-6 mt-2">
+                            <div class="row">
+                                <div class="col-12 pr-0 tx2">
+                                    <?php echo $lbnickname;  ?>
                                 </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="row">
-                                    <?php echo $txttel;  ?>
+                                <div class="col-12 pr-0">
+                                    <?php echo $txtnickname;  ?>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 mt-2">
-                        <div class="row">
-                            <div class="col-12 pr-0">
-                                <?php echo $lbemail;  ?>
-                            </div>
-                            <div class="col-12 pr-0">
-                                <?php echo $txtemail;  ?>
-                            </div>
-                        </div>
+                </div>
+                 <div class="col-12 mt-2 tx2">
+                    <div class="row">
+                            <?php echo $lbshop;  ?>
                     </div>
                 </div>
-            </div>
-            <div class="col-12 mt-2">
-                <div class="row">
-                    <?php echo $lbaddress;  ?>
-                </div>
-             </div>
-            <div class="col-12">
-                <div class="row">
-                    <?php echo $txtaddress;  ?>
-                </div>
-            </div>
-            <div class="col-12 pb-5">
-                <div class="row">
-                    <div class="col-4 mt-2">
-                        <div class="row">
-                            <div class="col-12 pl-0">
-                                <?php echo $lbprovince;  ?>
-                            </div>
-                            <div class="col-12 pl-0">
-                                <select id="selProvince" class="form-control">
-                                <option value=""> ------- เลือก ------ </option>
-                        <?php
-                                 $db->findAll("provinces");
-                                        while($cols = $db->moveNext_getRow('assoc')){
-                                        echo '<option name="customer_provinces" value="', $cols['provinces_id'], '">', $cols['provinces_name'],'</option>';
-                                        }
-                        ?>
-                                </select>
-                            </div>
-                        </div>
+                <div class="col-12">
+                    <div class="row">
+                            <?php echo $txtshop;  ?>
                     </div>
-                    <div class="col-4 mt-2">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="row">
-                                    <?php echo $lbdistrict;  ?>
+                </div>
+                <div class="col-12 mt-2">
+                    <div class="row">
+                        <div class="col-6 mt-2">
+                            <div class="row">
+                                <div class="col-12 tx2">
+                                    <div class="row">
+                                        <?php echo $lbtel;  ?>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="row">
+                                        <?php echo $txttel;  ?>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-12">
-                                <div class="row">
-                                    <select id="selDistricts" class="form-control">
+                        </div>
+                        <div class="col-6 mt-2">
+                            <div class="row">
+                                <div class="col-12 pr-0 tx2">
+                                    <?php echo $lbemail;  ?>
+                                </div>
+                                <div class="col-12 pr-0">
+                                    <?php echo $txtemail;  ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mt-2 tx2">
+                    <div class="row">
+                        <?php echo $lbaddress;  ?>
+                    </div>
+                 </div>
+                <div class="col-12">
+                    <div class="row">
+                        <?php echo $txtaddress;  ?>
+                    </div>
+                </div>
+                <div class="col-12 pb-5">
+                    <div class="row">
+                        <div class="col-4 mt-2">
+                            <div class="row">
+                                <div class="col-12 pl-0 tx2">
+                                    <?php echo $lbprovince;  ?>
+                                </div>
+                                <div class="col-12 pl-0">
+                                    <select id="selProvince" class="form-control">
                                     <option value=""> ------- เลือก ------ </option>
-                                    </select><span id="waitDistricts"></span>
+                            <?php
+                                     $db->findAll("provinces");
+                                            while($cols = $db->moveNext_getRow('assoc')){
+                                            echo '<option name="customer_provinces" value="', $cols['provinces_id'], '">', $cols['provinces_name'],'</option>';
+                                            }
+                            ?>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4 mt-2">
+                            <div class="row">
+                                <div class="col-12 tx2">
+                                    <div class="row">
+                                        <?php echo $lbdistrict;  ?>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="row">
+                                        <select id="selDistricts" class="form-control">
+                                        <option value=""> ------- เลือก ------ </option>
+                                        </select><span id="waitDistricts"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4 mt-2">
+                            <div class="row">
+                                <div class="col-12 pr-0 tx2">
+                                    <?php echo $lbsubdistrict;  ?>
+                                </div>
+                                <div class="col-12 pr-0">
+                                    <select id="selSubdistricts" class="form-control">
+                                    <option value=""> ------- เลือก ------ </option>
+                                    </select><span id="waitSubdistricts"></span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-4 mt-2">
-                        <div class="row">
-                            <div class="col-12 pr-0">
-                                <?php echo $lbsubdistrict;  ?>
-                            </div>
-                            <div class="col-12 pr-0">
-                                <select id="selSubdistricts" class="form-control">
-                                <option value=""> ------- เลือก ------ </option>
-                                </select><span id="waitSubdistricts"></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-             </div>
-            <div class="col-12">
-                <div class="row">
-                    <div class="col-4"></div>
-                    <input type="hidden" name="token" value="<?=$_SESSION['token']?>"/>
-                    <input type="hidden" name="sale_sale_id" value="<?=$sale_id?>"/>
-                    <div class="col-4">
-                        <div class="row">
-                            <div class="col-12">
-                                <?php echo $submit; ?>
+                 </div>
+                <div class="col-12 mb-5">
+                    <div class="row">
+                        <input type="hidden" name="token" value="<?=$_SESSION['token']?>"/>
+                        <input type="hidden" name="sale_sale_id" value="<?=$sale_id?>"/>
+                        <div class="col-4">
+                            <div class="row">
+                                <div class="col-12 pl-0">
+                                    <?php echo $submit; ?>
+                                </div>
                             </div>
                         </div>
+                        <div class="col-8"></div>
                     </div>
-                    <div class="col-4"></div>
                 </div>
             </div>
         </div>
-    </div>
+        <?php echo $form->close(); ?>
     <div class="col-4"></div>
+</div>
+</div>
+</div>
+</div>
 </div>
 <script>
 $(document).ready(function () {
