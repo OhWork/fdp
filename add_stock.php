bea69d6e22f8bac9fefddd262cdd4b5755527233
choatchaw <Choatchaw@live.com>
2019-08-21 23:00:35 +0700

    งง ค่าไม่มา

---
+++ add_stock.php
@@ -0,0 +1,84 @@
+<?php
+    $form = new form();
+    $lbname = new label('ชื่ออุปกรณ์การแพทย์');
+    $lbamount = new label('จำนวน');
+    $txtname = new textfield('mdeq_name','mdeq_name','form-control','','');
+    $txtamount = new textfield('mdeq_amount','','form-control','','');
+    $submit = new buttonok('บันทึก ลูกค้า','btnSubmit','btn btn-success col-12','');
+    $token = new tokens();
+    $tk = $token->openToken();
+     if(!empty($_GET['id'])){
+	//$r = $db->findByPK('mdeq','mdeq_id',$id)->executeRow();
+	//$txtmdeq->value = $r['mdeq_name'];
+    }
+   echo $form->open("","","col-12","insert_typemdeq.php","");
+   ?>
+<div class="row">
+    <div class="col-3"></div>
+    <div class="col-6 mt-5">
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <h3>เพิ่มจำนวนสินค้า</h3>
+                </div>
+            </div>
+            <div class="col-12 mt-4">
+                <div class="row">
+                        <?php echo $lbname;  ?>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="row">
+                        <?php echo $txtname; ?>
+                </div>
+            </div>
+
+ 
+
+            <div class="col-12 mt-5">
+                <div class="row">
+                    <div class="col-6"></div>
+                    <input type="hidden" name="stock_date" value="<?php echo date("Y-m-d"); ?>"/>
+                    <input type="hidden" name="token" value="<?php echo $_SESSION['token'] ?>"/>
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col-12 pr-0">
+                                <?php echo $submit; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-3"></div>
+</div>
+<script>
+ $(function() {
+
+        $( "#mdeq_name" ).autocomplete({ // ใช้งาน autocomplete กับ input text id=tags
+            minLength: 0, // กำหนดค่าสำหรับค้นหาอย่างน้อยเป็น 0 สำหรับใช้กับปุ่ใแสดงทั้งหมด
+            source: "get_mdeq.php", // กำหนดให้ใช้ค่าจากการค้นหาในฐานข้อมูล
+            open:function(){ // เมื่อมีการแสดงรายการ autocomplete
+                var valInput=$(this).val(); // ดึงค่าจาก text box id=tags มาเก็บที่ตัวแปร
+                if(valInput!=""){ // ถ้าไม่ใช่ค่าว่าง
+                    $(".ui-menu-item a").each(function(){ // วนลูปเรียกดูค่าทั้งหมดใน รายการ autocomplete
+                        var matcher = new RegExp("("+valInput+")", "ig" ); // ตรวจสอบค่าที่ตรงกันในแต่ละรายการ กับคำค้นหา
+                        var s=$(this).text();
+                        var newText=s.replace(matcher, "<b>$1</b>");    //      แทนค่าที่ตรงกันเป็นตัวหนา
+                        $(this).html(newText); // แสดงรายการ autocomplete หลังจากปรับรูปแบบแล้ว
+                    });
+                }
+            },
+            select: function( event, ui ) {
+                // สำหรับทดสอบแสดงค่า เมื่อเลือกรายการ
+              //console.log( ui.item ?
+               //   "Selected: " + ui.item.label :
+                //  "Nothing selected, input was " + this.value);
+                $("#ipzpo_address").val(ui.item.id); // เก็บ id ไว้ใน hiden element ไว้นำค่าไปใช้งาน
+                //setTimeout(function(){
+                 // $("#h_input_q").parents("form").submit(); // เมื่อเลือกรายการแล้วให้ส่งค่าฟอร์ม ทันที
+           //},500);
+            }
+        });
+        </script>
---
+++ get_mdeq.php
@@ -0,0 +1,23 @@
+<?php
+// ส่วนของการเชิ่อมต่อกับฐานข้อมูล  
+    include 'tools/db_tools.php';
+    include 'connect.php';
+    $q = $_GET["term"];
+
+$pagesize = 10; // จำนวนรายการที่ต้องการแสดง
+$table_db="ipzpo"; // ตารางที่ต้องการค้นหา
+$find_field="mdeq_name"; // ฟิลที่ต้องการค้นหา
+$rs = $db->conditions('mdeq',"locate('$q', $find_field ,mdeq_enable = '1') > 0 order by locate('$q', $find_field), $find_field limit $pagesize");
+//$rs = $db->specifytable("*",$table_db,"locate('$q', $find_field ,status_status_id = '1') > 0 order by locate('$q', $find_field), $find_field limit $pagesize")->execute();
+//while($row=mysqli_fetch_array($rs)) {
+while( $row = $rs->moveNext_getRow('array')){
+    $json_data[]=array(  
+        "id"=>$row['mdeq_id'],  
+        "label"=>$row['mdeq_name'],  
+        "value"=>$row['mdeq_name'],  
+    );    
+}  
+$json= json_encode($json_data);  
+echo $json;  
+exit;
+?>
