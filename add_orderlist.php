247c542ea5ff884777e7066c5daf4fcf20bf4c2c
choatchaw <Choatchaw@live.com>
2019-08-29 21:29:15 +0700

    form

--- add_orderlist.php
+++ add_orderlist.php
@@ -1,31 +1,203 @@
-
-
-<table id="myTable" border="1">
-    <thead>
-        <tr>
-            <td>เพิ่ม</td>
-            <td>สินค้า</td>
-        </tr>
-    </thead>
-        <tr>
-            <td><button type='button' onclick='addRow()'>+</button></td>
-            <td><input type='text'></td>
-            <td><input type="button" value="Delete" onclick="deleteRow(this)"></td>
-        </tr>
-</table>
-
-<script>
-function addRow() {
-  var table = document.getElementById("myTable");
-  var row = table.insertRow(2);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-   var cell3 = row.insertCell(2);
-  cell1.innerHTML = "<button type='button' onclick='addRow()'>+</button>";
-  cell2.innerHTML = "<input type='text'>";
-  cell3.innerHTML = "<input type='button' value='Delete' onclick='deleteRow(this)'>"
-  }
-function deleteRow(r) {
-  document.getElementById("myTable").deleteRow(0);
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+<link type="text/css" rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/pepper-grinder/jquery-ui.css" />
+<style type="text/css">
+.ui-autocomplete {  
+    padding-right: 5px;
+    max-height:200px !important;
+    overflow: auto !important;
+}  
+</style>
+</head>
+  
+<?php
+/// ส่วนของการเพิ่ม ลบ แก้ไข ข้อมูล
+if(isset($_POST['Submit'])){
+      
+    // ตรวจสอบค่า id หลักของข้อมูล ว่ามีข้อมูลหรือไม่
+    if(isset($_POST['h_item_id']) && count($_POST['h_item_id'])>0){
+        // แยกค่า id หลักของข้อมูลเดิม ถ้ามี เก็บเป็นตัวแปร array
+        $h_data_id_arr=explode(",",$_POST['h_all_id_data']);
+        foreach($_POST['h_item_id'] as $key_data=>$value_data){// วนลูป จัดการกับค่า id หลัก
+            if($value_data==""){ // ถ้าไม่มีค่า แสดงว่า จะเป็นการเพิ่มข้อมูลใหม่
+                $rs = $db->insert('orderlist',array(
+                'orderlist_mdeqcode' => $_POST['orderlist_mdeqcode'],
+                'emp_email' => $_POST['emp_email'],
+                'emp_password' => $_POST['emp_password'],
+                'emp_idcard' => $_POST['emp_idcard'],
+            ) );
+                $sql = "
+                    INSERT INTO tbl_data (
+                        data_id,
+                        data_text,
+                        data_select
+                    )
+                    VALUES (
+                        NULL ,
+                          '".$_POST['orderlist_mdeqcode'][$key_data]."',
+                            '".$_POST['data1'][$key_data]."'
+                    );                      
+                ";
+                $mysqli->query($sql);
+            }else{ // ถ้ามีค่าอยู่แล้ว ให้อัพเดท รายการข้อมูลเดิม โดยใช้ ค่า id หลัก
+                $sql = "
+                    UPDATE  tbl_data SET  
+                    data_text =  '".$_POST['data2'][$key_data]."',
+                    data_select =  '".$_POST['data1'][$key_data]."'
+                    WHERE data_id='".$value_data."' ;                   
+                ";
+                $mysqli->query($sql);
+            }
+        }
+          
+        // ตรวจสอบ id หลัก ค่าเดิม และค่าใหม่ เพื่อหาค่าที่ถูกลบออกไป
+        $h_data_id_arr_del = array_diff($h_data_id_arr, $_POST['h_item_id']);
+        if(count($h_data_id_arr_del)>0){ // ถ้ามี array ค่า id หลัก ที่จะถูกลบ
+            foreach($h_data_id_arr_del as $key_data=>$value_data){// วนลูป ลบรายการที่ไม่ต้องการ
+                $sql = "
+                    DELETE FROM tbl_data WHERE data_id='".$value_data."' ;   
+                ";
+                $mysqli->query($sql);
+            }
+        }
+          
+  
+    }
 }
+?>
+  
+<br />
+<br />
+<br />
+  
+<div style="margin:auto;">
+<form id="form1" name="form1" method="post" action="">
+<table id="myTbl" border="1" cellspacing="2" cellpadding="0">
+    <tr>
+    <th>เพิ่ม</th>
+    <th>อุปกรณ์การแพทย์</th>
+    <th>จำนวน</th>
+    <th>ราคา</th>
+    <th>ราคาปลอม</th>
+    <th>ลบ</th>
+    </tr>
+<?php
+$all_id_data = array();
+//$sql="SELECT * FROM tbl_data WHERE 1 ORDER BY data_id ";
+//$result = $mysqli->query($sql);
+
+if(isset($result) && $result->num_rows>0){
+?>
+<?php
+    while($row = $result->fetch_assoc()){
+        $all_id_data[].=$row['data_id'];
+?>
+  <tr class="firstTr">
+    <td>
+        <input type="text" class="text_data inputautofill" name="data2[]" id="data2[]" value="<?=$row['data_text']?>" />
+    </td>
+    <td>
+        <input type="text" class="text_data inputautofill" name="data2[]" id="data2[]" value="<?=$row['data_text']?>" />
+    </td>
+    <td>
+        <input type="text" class="text_data inputautofill" name="data2[]" id="data2[]" value="<?=$row['data_text']?>" />
+    </td>
+    <td>
+        <input type="text" class="text_data inputautofill" name="data2[]" id="data2[]" value="<?=$row['data_text']?>" />
+    </td>
+    <td>
+    <input name="h_item_id[]" type="hidden" id="h_item_id[]" value="<?=$row['data_id']?>" />
+    <input type="text" class="text_data inputautofill" name="data2[]" id="data2[]" value="<?=$row['data_text']?>" />
+    </td>
+    </tr>
+<?php } ?>    
+<?php }else{ ?>
+  <tr class="firstTr">
+    <td>
+    <button id="addRow" type="button"><i class="fas fa-plus"></i></button>
+    </td>
+    <td>
+    <input name="h_item_id[]" type="hidden" id="h_item_id[]" value="" />
+    <input type="text" class="text_data inputautofill" name="data2[]" id="data2[]" />
+    </td>
+    <td>
+    <input name="h_item_id[]" type="hidden" id="h_item_id[]" value="" />
+    <input type="text" class="text_data inputautofill" name="data2[]" id="data2[]" />
+    </td>
+    <td>
+    <input name="h_item_id[]" type="hidden" id="h_item_id[]" value="" />
+    <input type="text" class="text_data inputautofill" name="data2[]" id="data2[]" />
+    </td>
+    <td>
+    <input name="h_item_id[]" type="hidden" id="h_item_id[]" value="" />
+    <input type="text" class="text_data inputautofill" name="data2[]" id="data2[]" />
+    </td>
+       <td>
+            <button id="removeRow" type="button"><i class="fas fa-minus"></i></button>
+    </td>
+    </tr>
+ <?php } ?>   
+</table>
+<br />
+<table width="" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+    
+    <input name="h_all_id_data" type="hidden" id="h_all_id_data" value="<?=implode(",",$all_id_data)?>" />    
+    <input type="submit" name="Submit" id="Submit" value="Submit" /></td>
+  </tr>
+</table>
+</form>
+  
+  
+  
+<br />
+</div>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>      
+<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+<script type="text/javascript">
+$(function(){
+      
+    $("#addRow").click(function(){
+        // ส่วนของการ clone ข้อมูลด้วย jquery clone() ค่า true คือ
+        // การกำหนดให้ ไม่ต้องมีการ ดึงข้อมูลจากค่าเดิมมาใช้งาน
+        // รีเซ้ตเป็นค่าว่าง ถ้ามีข้อมูลอยู่แล้ว ทั้ง select หรือ input
+        $(".firstTr:eq(0)").clone(true) 
+        .find("input").attr("value","").end()
+        .find("select").attr("value","").end()
+        .appendTo($("#myTbl"));
+        var lastIndex=$(".inputautofill").size()-1; // หา index ของตัว input ล่าสุด
+        // สร้าง input element เพื่อที่จะไปแทนที่ตัวเก่า 
+        $($(".inputautofill:eq("+lastIndex+")")[0].outerHTML)
+        .insertAfter($(".inputautofill:eq("+lastIndex+")"))
+        .autocomplete({ // ใช้งาน autocomplete กับ input text id=tags
+            minLength: 0, // กำหนดค่าสำหรับค้นหาอย่างน้อยเป็น 0 สำหรับใช้กับปุ่ใแสดงทั้งหมด
+            source: "get_suggest.php", // กำหนดให้ใช้ค่าจากการค้นหาในฐานข้อมูล
+        });;
+        $(".inputautofill:eq("+lastIndex+")").remove(); // ลบตัวเดิมออก หลังจากแทนที่ตัวใหม่แล้ว
+    });
+    $("#removeRow").click(function(){
+        // // ส่วนสำหรับการลบ
+        if($("#myTbl tr").size()>2){ // จะลบรายการได้ อย่างน้อย ต้องมี 1 รายการ
+            $("#myTbl tr:last").remove(); // ลบรายการสุดท้าย
+        }else{
+            // เหลือ 1 รายการลบไม่ได้
+            alert("ต้องมีรายการข้อมูลอย่างน้อย 1 รายการ");
+        }
+    }); 
+      
+     $( ".inputautofill" ).autocomplete({ // ใช้งาน autocomplete กับ input text id=tags
+        minLength: 0, // กำหนดค่าสำหรับค้นหาอย่างน้อยเป็น 0 สำหรับใช้กับปุ่ใแสดงทั้งหมด
+        source: "get_suggest.php", // กำหนดให้ใช้ค่าจากการค้นหาในฐานข้อมูล
+    });
+ 
+  
+});
 </script>
+  
+  
+</body>
+</html>
--- inc_js.php
+++ inc_js.php
@@ -1,14 +1,6 @@
-<!--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDX-mBBK_AjTs-t2_NsF1Nv8Ax52MWJ1yM"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script src="jquery/dropdown2.js"></script>
-<script src="jquery/geolocation-marker.js"></script>
-<script src="jquery/owl.carousel.js"></script>
-<script src="jquery/select2.min.js"></script>
-<script src="jquery/feather-icon/feather.min.js"></script>
-<script src="jquery/jquery.star.rating.min.js"></script>
-<script src="https://apis.google.com/js/platform.js" async defer></script>-->
+<!-- JavaScript Includes -->
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
  <!-- Bootstrap core JavaScript-->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
