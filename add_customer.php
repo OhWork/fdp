5e29398890003d166d95690f185e9af7d06d644b
Firore <takbasic722@gmail.com>
2019-09-12 22:20:46 +0700

    up

--- add_customer.php
+++ add_customer.php
@@ -21,7 +21,7 @@
     $txtsubdistrict = new textfield('customer_subdistricts','','form-control','','');
     $txtdistrict = new textfield('customer_districts','','form-control','','');
     $txtprovince = new textfield('customer_provinces','','form-control','','');
-    $submit = new buttonok('ยินยัน','bs','btn btn-success col-12','');
+    $submit = new buttonok('บันทึก','bs','btn btn-success col-12','');
     $token = new tokens();
     $tk = $token->openToken();?>
 
@@ -29,44 +29,45 @@
   <div class="row">
         <div class="col-12 pt-3 tx3 card-header">
            <div class="row">
-               <h2 class="pl-3">ลงทะเบียนรายชื่อผู้ติดต่อ</h2>
+               <span class="pl-2 achf">ลงทะเบียนรายชื่อผู้ติดต่อ</span>
            </div>
         </div>
     <?php
     echo $form->open("form_reg","","col-12 tx1","insert_customer.php","");
     ?>
-        <div class="col-12">
-            <div class="row">
-                 <div class="col-12 pt-3 pb-2 tx3 fs1">
-                    <div class="row">
-                        <i class="far fa-building pt-1 tx3"></i>&nbsp<span>ข้อมูลสถานประกอบการ</span>
-                    </div>
-                 </div>
-                 <div class="col-12 mt-2 tx2">
-                     <div class="row">
-                         <div class="col-2 mt-2 tx2">
-                            <?php echo $lbshop;  ?><span class="tx4">*</span>
-                         </div>
-                            <?php echo $txtshop;  ?>
-                     </div>
-                </div>
-                <div class="col-12">
-                        
+<div class="col-12">
+        <div class="row">
+                <div class="col-12 pt-3 pb-2 tx3 fs1">
+                        <div class="row">
+                                <i class="far fa-building pt-1 tx3"></i>&nbsp<span>ข้อมูลสถานประกอบการ</span>
+                        </div>
                 </div>
                 <div class="col-12 mt-2 tx2">
-                    <?php echo $lbaddress;  ?><span class="tx4">*</span>
-                 </div>
-                <div class="col-12">
-                    <?php echo $txtaddress;  ?>
-                </div>
-                <div class="col-12">
-                    <div class="row">
-                        <div class="col-xl-4 col-lg-4 col-md-4 col-12 mt-2">
-                            <div class="row">
-                                <div class="col-12 prn tx2">
-                                    <?php echo $lbprovince;  ?><span class="tx4">*</span>
+                        <div class="row">
+                                <div class="w-100 ml-3 pt-1 tx2 actext">
+                                        <?php echo $lbshop;  ?><span class="tx4">*</span>
                                 </div>
-                                <div class="col-12 prn">
+                                <div class="w-100 ml-3 tx2 acpt1">
+                                        <?php echo $txtshop;  ?>
+                                </div>
+                        </div>
+                </div>
+                <div class="col-12 mt-2 tx2">
+                        <div class="row">
+                                <div class="w-100 ml-3 tx2 pt-1 actext">
+                                        <?php echo $lbaddress;  ?><span class="tx4">*</span>
+                                </div>
+                                <div class="w-100 ml-3 tx2 acpt2">
+                                        <?php echo $txtaddress;  ?>
+                                </div>
+                        </div>
+                </div>
+                <div class="col-12 mt-2">
+                        <div class="row">
+                                <div class="w-100 ml-3 pt-1 tx2 actext">
+                                        <?php echo $lbprovince;  ?><span class="tx4">*</span>
+                                </div>
+                                <div class="w-100 ml-3 tx2 acpt3">
                                     <select id="selProvince" name="customer_provinces" class="form-control">
                                     <option value=""> ----- เลือก ----- </option>
                             <?php
@@ -77,103 +78,91 @@
                             ?>
                                     </select>
                                 </div>
-                            </div>
                         </div>
-                        <div class="col-xl-4 col-lg-4 col-md-4 col-12 mt-2">
-                            <div class="row">
-                                <div class="col-12 tx2">
+                </div>
+                <div class="col-12 mt-2">
+                        <div class="row">
+                                <div class="w-100 ml-3 pt-1 tx2 actext">
                                         <?php echo $lbdistrict;  ?><span class="tx4">*</span>
                                 </div>
-                                <div class="col-12">
+                                <div class="w-100 ml-3 tx2 acpt3">
                                         <select id="selDistricts" name="customer_districts"class="form-control">
                                         <option value=""> ----- เลือก ----- </option>
                                         </select><span id="waitDistricts"></span>
                                 </div>
-                            </div>
                         </div>
-                        <div class="col-xl-4 col-lg-4 col-md-4 col-12 mt-2">
-                            <div class="row">
-                                <div class="col-12 pln tx2">
-                                    <?php echo $lbsubdistrict;  ?><span class="tx4">*</span>
+                </div>
+                 <div class="col-12 mt-2">
+                        <div class="row">
+                                <div class="w-100 ml-3 pt-1 tx2 actext">
+                                        <?php echo $lbsubdistrict;  ?><span class="tx4">*</span>
                                 </div>
-                                <div class="col-12 pln">
-                                    <select id="selSubdistricts" name="customer_subdistricts" class="form-control">
-                                    <option value=""> ----- เลือก ----- </option>
-                                    </select><span id="waitSubdistricts"></span>
+                                <div class="w-100 ml-3 tx2 acpt3">
+                                        <select id="selSubdistricts" name="customer_subdistricts" class="form-control">
+                                        <option value=""> ----- เลือก ----- </option>
+                                        </select><span id="waitSubdistricts"></span>
                                 </div>
-                            </div>
                         </div>
-                    </div>
-                 </div>
+                </div>
                 <div class="col-12 mt-3 bdac">
                 </div>
                 <div class="col-12 pt-3 pb-2 tx3 fs1">
-                    <div class="row">
-                        <i class="fas fa-users pt-1 tx3"></i>&nbsp<span>ข้อมูลผู้ติดต่อ</span>
-                    </div>
-                 </div>
-                <div class="col-12 mt-3 tx2">
-                        <?php echo $lbname;  ?><span class="tx4">*</span>
-                </div>
-                <div class="col-12">
-                        <?php echo $txtname;  ?>
-                </div>
-                <div class="col-12">
-                    <div class="row">
-                        <div class="col-6 mt-2">
-                            <div class="row">
-                                <div class="col-12 pr-0 tx2">
-                                    <?php echo $lbnickname;  ?><span class="tx4">*</span>
-                                </div>
-                                <div class="col-12 pr-0">
-                                    <?php echo $txtnickname;  ?>
-                                </div>
-                            </div>
+                        <div class="row">
+                                <i class="fas fa-users pt-1 tx3"></i>&nbsp<span>ข้อมูลผู้ติดต่อ</span>
                         </div>
-                         <div class="col-6 mt-2">
-                            <div class="row">
-                                <div class="col-12 tx2">
-                                    <?php echo $lbposition;  ?>
-                                </div>
-                                <div class="col-12">
-                                    <?php echo $txtposition;  ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
                 <div class="col-12 mt-2">
-                    <div class="row">
-                        <div class="col-6 mt-2">
-                            <div class="row">
-                                <div class="col-12 tx2">
-                                    <?php echo $lbtel;  ?><span class="tx4">*</span>
+                        <div class="row">
+                                <div class="w-100 ml-3 pt-1 tx2 actext">
+                                        <?php echo $lbname;  ?><span class="tx4">*</span>
                                 </div>
-                                <div class="col-12 pr-0">
-                                    <?php echo $txttel;  ?>
+                                <div class="w-100 ml-3 tx2 acpb1">
+                                        <?php echo $txtname;  ?>
                                 </div>
-                            </div>
+                                <div class="w-100 ml-3 pt-1 tx2 actext3">
+                                        <?php echo $lbnickname;  ?><span class="tx4">*</span>
+                                </div>
+                                <div class="w-100 ml-3 tx2 acpb2">
+                                        <?php echo $txtnickname;  ?>
+                                </div>
                         </div>
-                        <div class="col-6 mt-2">
-                            <div class="row">
-                                <div class="col-12 pr-0 tx2">
-                                    <?php  echo $lbemail;  ?><span class="tx4">*</span>
-                                </div>
-                                <div class="col-12">
-                                    <?php echo $txtemail;  ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
+                <div class="col-12 mt-2">
+                        <div class="row">
+                                <div class="w-100 ml-3 pt-1 tx2 actext">
+                                        <?php echo $lbposition;  ?><span class="tx4">*</span>
+                                </div>
+                                <div class="w-100 ml-3 tx2 acpb3">
+                                        <?php echo $txtposition;  ?>
+                                </div>
+                                <div class="w-100 ml-3 pt-1 tx2 actext4">
+                                        <?php echo $lbtel;  ?><span class="tx4">*</span>
+                                </div>
+                                <div class="w-100 ml-3 tx2 acpb4">
+                                        <?php echo $txttel;  ?>
+                                </div>
+                        </div>
+                </div>
+                <div class="col-12 mt-2">
+                        <div class="row">
+                                <div class="w-100 ml-3 pt-1 tx2 actext">
+                                        <?php  echo $lbemail;  ?><span class="tx4">*</span>
+                                 </div>
+                                <div class="w-100 ml-3 tx2 acpb3">
+                                        <?php echo $txtemail;  ?>
+                                </div>
+                        </div>
+                </div>
+        </div>
+</div>
 <!--                <div class="col-12 mt-3 bdac">
                 </div>-->
                 <div class="col-12 mt-3 mb-3">
                     <div class="row">
-                        <div class="col-xl-10 col-lg-10 col-md-10 col-9"></div>
+                        <div class="col-xl-11 col-lg-10 col-md-10 col-9"></div>
                         <input type="hidden" name="token" value="<?=$_SESSION['token']?>"/>
                         <input type="hidden" name="emp_emp_id" value="<?=$_SESSION['emp_id']?>"/>
-                        <div class="col-xl-2 col-lg-2 col-md-2 col-3">
+                        <div class="col-xl-1 col-lg-2 col-md-2 col-3">
                             <div class="row">
                                 <div class="col-12 pl-0">
                                     <?php echo $submit; ?>
