ec48efe685be53c5db8a49d6aa4ff75b8c6da5ce
choatchaw <Choatchaw@live.com>
2019-09-19 20:41:14 +0700

    aaa

--- get_mdeqfororderlist.php
+++ get_mdeqfororderlist.php
@@ -5,18 +5,18 @@
     $q = $_GET["term"];
 
 $pagesize = 10; // จำนวนรายการที่ต้องการแสดง
-$find_field="mdeq_barcode"; // ฟิลที่ต้องการค้นหา
+$find_field="mdeq_code"; // ฟิลที่ต้องการค้นหา
 $rs = $db->conditions('mdeq',"locate('$q', $find_field ,mdeq_enable = '1') > 0 order by locate('$q', $find_field), $find_field limit $pagesize");
 //$rs = $db->specifytable("*",$table_db,"locate('$q', $find_field ,status_status_id = '1') > 0 order by locate('$q', $find_field), $find_field limit $pagesize")->execute();
 //while($row=mysqli_fetch_array($rs)) {
 while( $row = $rs->moveNext_getRow('array')){
     $json_data[]=array(
         "id"=>$row['mdeq_id'],
-        "label"=>$row['mdeq_barcode']."(".$row['mdeq_name'].")",
+        "label"=>$row['mdeq_code']."(".$row['mdeq_name'].")",
         "name"=>$row['mdeq_name'],
         "unit"=>$row['mdeq_unit'],
         "price"=>$row['mdeq_price'],
-        "code" =>$row['mdeq_barcode']
+        "code" =>$row['mdeq_code']
     );
 }
 $json= json_encode($json_data);
